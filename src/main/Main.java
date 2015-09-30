3d873561f4f80188df1e3a3b839e7ed52e6e150a
Kaleidophon <dennisulmer@dhcp664-222.laptop-wlc.uni-heidelberg.de>
2015-09-30 19:47:09 +0200

    Nachtrag

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,27 +1,14 @@
 package main;
 
-import inout.indexing.HexadecimalIndexing;
-import inout.indexing.Indexing;
-import inout.paths.Path;
-import inout.paths.PathHandler;
-import utilities.eval.Evaluation;
-import languagemodel.*;
-import languagemodel.calc.MaximumLikelihoodEstimation;
-import languagemodel.model.LanguageModel;
-
 public class Main {
 	
 	public static void main(String[] args) {
 
-		//PathHandler ph = new PathHandler("./rsc/patahs.xml");
-		//Path p = ph.getFirstPathWithAttributes("zipped hexadecimal probability indexing 5 read");
-		//Indexing<Double> index = new HexadecimalIndexing<>(p.getDirectory(), ph.getFirstPathWithAttributes("zipped lexicon").getDirectory(), p.isZipped());
-		
 		// Calculation
-		LanguageModel lm = new LanguageModel(5, "./rsc/paths.xml", new MaximumLikelihoodEstimation(), "fast back-off", true);
-		lm.calculate();
-		System.out.println("");
-		lm.calculateParallelized(1, 3);
+		//LanguageModel lm = new LanguageModel(5, "./rsc/paths.xml", new MaximumLikelihoodEstimation(), "fast back-off", true);
+		//lm.calculate();
+		//System.out.println("");
+		//lm.calculateParallelized(1, 3);
 		
 		// Evaluation
 		//LanguageModel lm = new LanguageModel(3, "./rsc/paths.xml", "fast back-off", true);
